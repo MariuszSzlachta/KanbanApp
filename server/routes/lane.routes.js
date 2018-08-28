3c8c7a24f8bc553bd54b404eff0d13d0edd0f629
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-08-28 15:13:25 +0200

    Add lane router
    Add lane router for adding lanes using post on /lanes endpoint

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -3,5 +3,7 @@ import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
 
+// Add new lane
+router.route('/lanes').post(LaneController.addLane);
 
 export default router;
