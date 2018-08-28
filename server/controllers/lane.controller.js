e6df42b53a1e440b980bf009daea1ff8da2b416b
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-08-28 15:44:16 +0200

    Add API foos: "Add lane" and "Get all lanes"

--- server/controllers/lane.controller.js
+++ server/controllers/lane.controller.js
@@ -1,10 +1,17 @@
 import Lane from '../models/lane';
 import uuid from 'uuid';
 
-export function getSomething(req, res) {
-  return res.status(200).end();
+// Get all lanes
+export function getLanes(req, res) {
+  Lane.find().exec((err, lanes) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.json({ lanes });
+  });
 }
 
+// Add lane
 export function addLane(req, res) {
   if (!req.body.name) {
     res.status(403).end();
