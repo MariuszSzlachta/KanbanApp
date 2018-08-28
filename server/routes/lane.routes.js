86f96382aa10c55cf60d6c6a95324c051efa6d25
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-08-28 15:49:05 +0200

    Add 'add lane' router

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import * as LaneController from '../controllers/lane.controller';
 
 const router = new Router();
+// Get all lanes
+router.route('/lanes').get(LaneController.getLanes);
 
 // Add new lane
 router.route('/lanes').post(LaneController.addLane);
