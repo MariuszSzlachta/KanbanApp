ecb84aafbceb6c8328fc6cb04e08bd03dcc8d728
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-08-31 13:03:47 +0200

    Add notes action into lane reducer

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,5 +1,7 @@
 // Import Actions
 import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
+import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
+
 
 // Initial State
 const initialState = [];
@@ -18,6 +20,25 @@ const lanes = (state = initialState, action) => {
       });
     case DELETE_LANE:
       return state.filter(lane => lane.id !== action.laneId);
+
+    case CREATE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes, action.note.id];
+          return { ...lane, notes };
+        }
+        return lane;
+      });
+
+    case DELETE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = state.filter(note => note.id !== action.noteId);
+          return { ...lane, notes };
+        }
+        return lane;
+      });
+
     default:
       return state;
   }
