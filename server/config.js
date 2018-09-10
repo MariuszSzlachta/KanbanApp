2ed37e049b6d1a63f5ccc311173f8705ab83c856
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-09-10 16:08:59 +0200

    Change port

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
   mongoURL: 'mongodb://admin:admin1234@ds249092.mlab.com:49092/kanban' || process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
-  port: 49094 || process.env.PORT || 8000,
+  port: process.env.PORT || 8000,
 };
 
 export default config;
